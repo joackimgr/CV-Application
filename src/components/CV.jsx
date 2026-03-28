df9af9b26f07a3332754cb6b636d7635c2c48318
Ioakeim <joackim.gr@gmail.com>
2026-03-28 21:46:21 +0200

    Added a CV Template that changed in real time

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -15,6 +15,35 @@ function CV() {
                 <Education school = {edu.school} title = {edu.title} date = {edu.date} onEduChange = {setEdu}/>
                 <Experience company = {exp.company} position = {exp.position} responsibilities = {exp.responsibilities} dateFrom = {exp.dateFrom} dateTo = {exp.dateTo} onExpChange = {setExp}/>
             </div>
+            <div className='right'>
+                <div className='infoDisplay'>
+                    <h1>{info.name}</h1>
+                    <div className='contact'>
+                        <p>Email: {info.email}</p>
+                        <p>Phone: {info.phone}</p>
+                    </div>
+                </div>
+                <div className="educationDisplay">
+                    <h2>Education</h2>
+                    <div className="school">
+                        <h4>{edu.school}, {edu.title}</h4>
+                        <p>{edu.date}</p>
+                    </div>
+                </div>
+                <div className="experienceDisplay">
+                    <h2>Experience</h2>
+                    <div className="company">
+                        <div className="companyLeft">
+                            <h3>{exp.position}</h3>
+                            <h4>{exp.company}</h4>
+                            <p>{exp.responsibilities}</p>
+                        </div>
+                        <div className="companyRight">
+                            <p>{exp.dateFrom} - {exp.dateTo}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
